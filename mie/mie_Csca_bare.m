f97ad5101217fd4ba74aff42e5c9f66eff379a53
rbsasse <40582866+rbsasse@users.noreply.github.com>
2018-06-26 13:12:47 +1200

    Delete mie_Csca_bare.m

--- mie/mie_Csca_bare.m
+++
@@ -1,9 +0,0 @@
-function Csca = mie_Csca_bare(stMiepq, kn)
-%% mie_Csca
-% Calculates the scattering cross-section
-
-Csca = 1/kn^2*sum(abs(stMiepq.pmn).^2 + ...
-    abs(stMiepq.qmn).^2, 2);
-
-
-end
